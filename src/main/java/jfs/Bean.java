8d9d204ef73dc4a688f511418b217e6c370a44e3
iainaharilanto <iainaharilanto@yahoo.fr>
2022-04-03 21:10:31 +0400

    tp3: templates

--- src/main/java/jfs/Bean.java
+++ src/main/java/jfs/Bean.java
@@ -9,8 +9,8 @@ import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 
-//@Named(value = "bean")
-@Named(value = "bean2")
+@Named(value = "bean")
+//@Named(value = "bean2")
 @RequestScoped
 public class Bean {
 
@@ -34,6 +34,6 @@ public class Bean {
     }
     
     public String afficher() {
-        return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+        return "affichage__1?nb="+ nombre + "&amp;faces-redirect=true";
     }
 }
